37731ce0a52c2deca60783a4c2c0d17a07d73903
Alvaro-R <alvaro.roman@github.com>
2024-12-14 19:49:26 +0100

    Edit globals.sh

--- scripts/src/lib/globasl.sh
+++ scripts/src/lib/globasl.sh
@@ -9,11 +9,10 @@ VSCODE_CONFIG_FILE="settings.json"
 VSCODE_EXTENSIONS_FILE="$HOME/.dotfiles/.config/vscode/extensions.txt"
 
 # SSH PATH VARIABLES
-export SSH_PATH="$HOME/.ssh"
-export SSH_KEYS_PATH="$SSH_PATH/keys"
-export SSH_CONFIG_FILE="$SSH_PATH/config"
+SSH_PATH="$HOME/.ssh"
+SSH_KEYS_PATH="$SSH_PATH/keys"
+SSH_CONFIG_FILE="$SSH_PATH/config"
 
 # CONFIGURATION FILES
-export DOTFILES_DIR="$HOME/dotfiles"
-export NIX_CONFIG_DIR="$HOME/.nix-config"
-export NIX_DARWIN_DIR="$HOME/.nix-darwin"
+NIX_CONFIG_DIR="$HOME/.config/nix/nix.conf"
+NIX_DARWIN_DIR="$HOME/.config/nix/nix-darwin"
