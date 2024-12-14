da35b6dee14ad127355a15fdf4cdf4373a32cba8
Alvaro-R <alvaro.roman@github.com>
2024-12-14 14:00:25 +0100

    Add VSCode symlink

---
+++ scripts/symlink-vscode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ruta de la configuración de VSCode
+DOTFILES_DIR="$HOME/.dotfiles"
+VSCODE_CONFIG_DIR="$HOME/Library/Application Support/Code/User"
+VSCODE_CONFIG_FILE="settings.json"
+
+# Crear el directorio de configuración de VSCode si no existe
+mkdir -p "$VSCODE_CONFIG_DIR"
+
+# Eliminar el archivo de configuración existente si existe
+if [ -f "$VSCODE_CONFIG_DIR/$VSCODE_CONFIG_FILE" ]; then
+  echo "Eliminando el archivo existente $VSCODE_CONFIG_FILE"
+  rm "$VSCODE_CONFIG_DIR/$VSCODE_CONFIG_FILE"
+fi
+
+# Crear el symlink
+echo "Creando symlink para $VSCODE_CONFIG_FILE en $VSCODE_CONFIG_DIR"
+ln -s "$DOTFILES_DIR/vscode/$VSCODE_CONFIG_FILE" "$VSCODE_CONFIG_DIR/$VSCODE_CONFIG_FILE"
+
+# Confirmar el symlink
+if [ -L "$VSCODE_CONFIG_DIR/$VSCODE_CONFIG_FILE" ]; then
+  echo "Symlink creado exitosamente."
+else
+  echo "Hubo un problema al crear el symlink."
+fi
