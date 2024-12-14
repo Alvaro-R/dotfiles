d46cc09743b88246745e3ed0a2a8f89c4bdb0303
Alvaro-R <alvaro.roman@github.com>
2024-12-14 14:34:19 +0100

    Create bashly script

---
+++ scripts/src/install_vscode_extension_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/install_vscode_extension_command.sh'"
+echo "# code for 'dotfiles install-vscode-extension' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
---
+++ scripts/src/stow_dotfiles_command.sh
@@ -0,0 +1,4 @@
+echo "# this file is located in 'src/stow_dotfiles_command.sh'"
+echo "# code for 'dotfiles stow-dotfiles' goes here"
+echo "# you can edit it freely and regenerate (it will not be overwritten)"
+inspect_args
